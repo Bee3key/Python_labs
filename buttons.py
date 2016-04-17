8e30267e6a999934abadb2233ef7064bd0e26450
Bee3key <trx3Gzix@gmail.com>
2016-04-17 16:52:07 +0300

    del

--- buttons.py
+++
@@ -1,5 +0,0 @@
-from functools import partial
-mybutton = partial(Button, root, fg="black",bg="white",font="times",size="12")
-b1 = mybutton(text="Ok") 
-b2 = mybutton(text="Cancel") 
-b3 = mybutton(text="Restart")
